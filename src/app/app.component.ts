4c153f4087cd03576633b1fcb936b17aa2fa8c22
tom.vrijmoet <tom.vrijmoet@ns.nl>
2023-12-06 17:29:39 +0100

    Added some error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { PostsService } from './posts.service';
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching: boolean = false;
+  error = null;
 
   constructor(private http: HttpClient,
               private postsService: PostsService) {}
@@ -35,6 +36,8 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, error => {
+      this.error = error.message;
     });
   }
 
