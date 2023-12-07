c1b56d9e1f0877ccf8b0083c47ccb2bf6913e4d5
tom.vrijmoet <tom.vrijmoet@ns.nl>
2023-12-07 16:03:51 +0100

    Making changes to requests in the interceptor

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,8 +4,11 @@ export class AuthInterceptorService implements HttpInterceptor {
     
     // This code will run right before a requests leaves our application
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        
         console.log("The request is on its way");
-        return next.handle(req);
+        console.log(req.url);
+        const modifiedRequest = req.clone({
+            headers: req.headers.append('Auth', 'xyz'),
+        })
+        return next.handle(modifiedRequest);
     }
 }
